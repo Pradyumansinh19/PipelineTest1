864969b51a20a838b475066dfd35e51ca7f60874
Pradyumansinh <viholpradyumansinh@gmail.com>
2021-09-28 11:05:24 +0530

    First Commit

---
+++ src/test/java/PipelineTest1/AppTest.java
@@ -0,0 +1,17 @@
+package PipelineTest1;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+ 
+	@Test
+	public void PP1()
+	{
+		System.out.println("This is pipeline1");
+	}
+    
+}
